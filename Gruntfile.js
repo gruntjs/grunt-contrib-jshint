ab199c6f06cdb5ad9099bb56b580a0adf090fc50
Tyler Kellen <tyler@sleekcode.net>
2012-10-03 09:30:50 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+/*
+ * grunt-contrib-jshint
+ * https://github.com/gruntjs/grunt-contrib-jshint
+ *
+ * Copyright (c) 2012 "Cowboy" Ben Alman
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<config:nodeunit.tests>'
+      ],
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true
+      }
+    }
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint']);
+
+};
---
+++ README.md
@@ -0,0 +1,3 @@
+# grunt-contrib-jshint (unreleased, for grunt v0.4.0a)
+
+> Validate files with JSHint
