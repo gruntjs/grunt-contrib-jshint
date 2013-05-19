f3d74eb72bec521a83263f09eaffe3ac11e720f5
xzyfer <xzyfer@gmail.com>
2013-05-19 13:42:17 +1000

    Execute the reporter once. Fixes GH-54.

--- tasks/lib/jshint.js
+++ tasks/lib/jshint.js
@@ -194,22 +194,18 @@ exports.init = function(grunt) {
     cliOptions.config = options;
     grunt.verbose.writeflags(options, 'JSHint options');
 
-    // Run JSHint on each file and collect results/data
+    // Run JSHint on all file and collect results/data
     var allResults = [];
     var allData = [];
-    grunt.util.async.forEach(files, function(filepath, next) {
-      var cliopts = grunt.util._.clone(cliOptions);
-      cliopts.args = [filepath];
-      cliopts.reporter = function(results, data) {
-        reporter(results, data);
-        allResults = allResults.concat(results);
-        allData = allData.concat(data);
-        next();
-      };
-      jshintcli.run(cliopts);
-    }, function() {
-      done(allResults, allData);
-    });
+    var cliopts = grunt.util._.clone(cliOptions);
+    cliopts.args = files;
+    cliopts.reporter = function(results, data) {
+      reporter(results, data);
+      allResults = allResults.concat(results);
+      allData = allData.concat(data);
+    };
+    jshintcli.run(cliopts);
+    done(allResults, allData);
   };
 
   return exports;
