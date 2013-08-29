95aee7ba76bfe8f04c2b23814d6c98e7e9d5b1b6
teppeis <teppeis@gmail.com>
2013-08-29 13:40:56 +0900

    Use loadConfig of jshint/cli.js to allow comments in .jshintrc. Closes GH-98.

--- tasks/lib/jshint.js
+++ tasks/lib/jshint.js
@@ -186,7 +186,7 @@ exports.init = function(grunt) {
 
     // Read JSHint options from a specified jshintrc file.
     if (options.jshintrc) {
-      options = grunt.file.readJSON(options.jshintrc);
+      options = jshintcli.loadConfig(options.jshintrc);
       delete options.jshintrc;
     }
 
